ad2c6772bf12259a06e232f65cc7a93c4ce986fe
hyemin976 <25s10727@guro.sen.hs.kr>
2026-07-20 13:13:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,27 @@ with st.sidebar:
 
 st.title("🩱AI 코디 메이커")
 st.write("사이드바에서 날씨를 먼저 선택하고 코디를 시작하세요!")
+
+st.header("👕아이템 조합하기")
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("상의")
+    top_type = st.radio("종류", ["후드티", "셔츠", "맨투맨", "반팔 티셔츠"])
+    top_color = st.seclect_slider("색상 톤", options=["밝음", "무난함", "어두움"])
+    with col2:
+        st.subheader("하의")
+    bottom_type = st.radio("종류", ["청바지", "슬랙스", "트레이닝 팬츠", "반바지"])
+    bottom_color = st.seclect_slider("핏(Fit)", options=["슬림", "레귤러", "오버핏"])
+
+st.header("디테일 추가")
+tab1, tab2 = st.tabs(["신발", "악세서리"])
+with tab1:
+    st.write("오늘의 발걸음을 책임질신발:")
+    shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두", "슬리퍼"])
+    with st.expander("신발 선택 팁 보기"):
+        st.info("너무 튀는 신발은 지양하도록 해요!")
+with tab2:
+     st.write("포인트 아이템:")
+     acc = st.multiselect("악세사리 추가", ["모자", "안경", "목걸이", "가방"])
+     with st.expander("액세서리 스타일 팁 보기"):
+       st.warning("너무 많은 액세서리는 투머치가 될 수 있어요.")
