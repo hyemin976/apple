fdbcd11fe2649f9309b541c77c9e9955d8398b08
hyemin976 <25s10727@guro.sen.hs.kr>
2026-07-20 11:19:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ tone = st.selectbox("반", ["1", "2", "3", "4", "5"])
 ai_speed = st.select_slider("난이도",options=["매우 느림", "느림", "보통", "빠름", "실시간"],value="보통")
 creativity = st.slider("점수", 0, 100, 50)
 question = st.text_area("소감", placeholder="소감입니다.")
-age = st.number_input("나이를 입력하세요", min_value=1, max_value=100, value=17)
-question = st.text_area("AI에게 보낼 질문을 입력하세요", placeholder="여기에 소감을 작성해 주세요.")
 
 if st.button("질문 전송하기"):
     if agree:
