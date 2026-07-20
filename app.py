4cd03dc217aae0dd15b777e8b1c30148069eb6b5
hyemin976 <25s10727@guro.sen.hs.kr>
2026-07-20 11:36:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import streamlit as st
 
-st.header("앱 UI 만들기")
-user_id = st.text_input("이름", placeholder="이름")
-ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
-tone = st.selectbox("반", ["1", "2", "3", "4", "5"])
-ai_speed = st.select_slider("난이도",options=["매우 느림", "느림", "보통", "빠름", "실시간"],value="보통")
-creativity = st.slider("점수", 0, 100, 50)
-question = st.text_area("소감", placeholder="소감입니다.")
+with st.sidebar:
+    st.header("프로필")
+    user_name = st.text_input("닉네임")
+    weather = st.selectbox("오늘 날씨", ["맑음", "흐림", "비/눈", "매우 추움"])
+    st.markdown("---")
+    st.info(f"반가워요, {user_name}님! 오늘 날씨는 '{weather}'이네요.")
 
-if st.button("질문 전송하기"):
-    if agree:
-        st.success(f"이름: {user_id}/{ai_model}학년/{tone}반/{ai_speed}, 소감: {question}")
-        st.markdown(f"""
-        **점수:** {creativity}
-        """)
-    else:
-        st.error("⚠️ 동의 항목에 체크해야 전송이 가능합니다.")
+st.title("AI 코디 메이커")
+st.write("사이드바에서 날씨를 먼저 선택하고 코디를 시작하세요!")
