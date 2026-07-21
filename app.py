31a76fe64fc0732fa5228229b09e4b481b059a61
hyemin976 <25s10727@guro.sen.hs.kr>
2026-07-21 09:46:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,23 @@ def add_todo():
         st.toast("할 일이 추가되었습니다!")
         st.session_state.todo_input = ""
 
+@st.dialog("오늘의 다짐 수정")
+def edit_motto():
+        motto = st.text_input("나의 한 줄 좌우명을 적어주세요")
+        if st.button("다짐 저장"):
+            st.session_state.user_motto = motto
+            st.session_state.motto_updated = True
+            st.rerun()
+
 def page_motto():
     st.header("📣 1. 오늘의 다짐")
-    motto = st.text_input("나의 한 줄 좌우명을 적어주세요")
-    if st.button("다짐 저장"):
-        st.session_state.user_motto = motto
-        st.success("좌우명이 등록되었습니다!")
+    st.info(f"현재 다짐: {st.session_state.user_motto}")
+    if st.button("다짐 수정하기"):
+        edit_motto()
+    if st.session_state.motto_updated:
+            st.success("새로운 좌우명이 등록되었습니다!")
+            st.session_state.motto_updated = False
+            st.success("좌우명이 등록되었습니다!")
     st.markdown("---")
 
 def page_todo():
